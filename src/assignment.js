b085377070b3490d6c93236cdc9ba66042caf001
Edoror Victor <143561969+EdororVictor@users.noreply.github.com>
2023-08-30 16:38:14 +0000

    Implemented the sumOfNumbersTo function in src/assignment.js. The function calculates and returns the sum of numbers from 1 to the specified destination. Removed comment related to Challenge - 1. Added the countEvenNumbersWithin function to src/assignment.js. This function determines and returns the count, sum, and arrayOfEvenNumbers for even numbers within a specified range. Removed comment regarding Challenge - 2.

--- src/assignment.js
+++ src/assignment.js
@@ -14,13 +14,16 @@ const assignment = {};
  * @param {Number} destination is the stopping number
  * @returns number the sum of the numbers from 1 to destination
  */
+
 function sumOfNumbersTo(destination) {
     let sum = 0;
+    for (let i = 1; i <= destination; i++) {
+        sum += i;
     return sum
 }
 
 
-// assignment.sumOfNumbersTo = sumOfNumbersTo;
+assignment.sumOfNumbersTo = sumOfNumbersTo;
 
 /**
  * Challenge - 2
@@ -39,6 +42,16 @@ function countEvenNumbersWithin(destination) {
     let count = 0;
     let arrayOfEvenNumbers = [];
 
+    for(i = 1; i<= destination; i++){
+        if(i % 2 === 0){
+            count++;
+            sum +=i;
+            arrayOfEvenNumbers.push(i);
+
+        }
+    }
+}
+
     return {
         // property value shorthand
         // when the property name and the value name are the same
@@ -49,7 +62,7 @@ function countEvenNumbersWithin(destination) {
     };
 }
 
-// assignment.countEvenNumbersWithin = countEvenNumbersWithin;
+assignment.countEvenNumbersWithin = countEvenNumbersWithin;
 
 /**
  * Challenge - 3
